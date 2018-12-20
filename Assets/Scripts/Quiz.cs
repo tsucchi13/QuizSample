ee1b0d6508f75113bd77b492b9a87e1c5f8f9755
Tsubasa Tsuchiya <t.tsuchiya83@gmai.com>
2018-12-20 16:58:38 +0900

    initial commit

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,15 @@
+﻿public class Quiz
+{
+    public string question;
+    public string answer;
+    public int answerIndex;
+
+    public Quiz(string _question, string _answer, int _answerIndex)
+    {
+        question = _question;
+        answer = _answer;
+        answerIndex = _answerIndex;
+
+    }
+
+}
